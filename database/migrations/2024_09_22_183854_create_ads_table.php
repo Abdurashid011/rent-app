ad0a83b71a7a96d194628821ea01dac226e7633b
Abdurashid011 <abdumajidovabdurashid1504@gmail.com>
2024-10-29 17:44:29 +0500

    U

--- database/migrations/2024_09_22_183854_create_ads_table.php
+++ database/migrations/2024_09_22_183854_create_ads_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->foreignId('status_id')->constrained();
             $table->timestamps();
         });
+
     }
 
     public function down(): void
